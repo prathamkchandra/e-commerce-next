4369dfe5a007c251e63654cd42ddcf957f6175a5
prathamkchandra <prathamkappu565@gmail.com>
2025-06-04 23:27:16 +0530

    Add cart functionality with Zudstand store and integrate UI components

--- types/product.d.ts
+++ types/product.d.ts
@@ -1,11 +1,13 @@
 export interface IProduct {
-  id: number;
-  localizeInfos: { title: Record<string, any> };
-  price: number | null;
+  id: any;
+
+  localizeInfos: { title: any };
+
+  price: any;
+
   attributeValues: {
-    p_description: { value: { htmlValue: string }[] };
-    p_price: { value: number };
-    p_image: { value: { downloadLink: string } };
-    p_title: { value: string };
+    p_image: { value: { downloadLink: any } };
+    p_price: any;
+    p_title: { value: any };
   };
 }
