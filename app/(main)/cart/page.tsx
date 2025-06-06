a7995ec4a23481df7faf11b501438d4eef18467b
prathamkchandra <prathamkappu565@gmail.com>
2025-06-06 17:32:03 +0530

    fixed typed errors

--- app/(main)/cart/page.tsx
+++ app/(main)/cart/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 
 import { Input } from '@/components/ui/input';
+
 import Image from 'next/image';
 
 
